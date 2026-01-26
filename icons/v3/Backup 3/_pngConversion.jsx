be9896dd8b90033a9b4d2c4936e7d25faeaf202a
Dein Name <deine-email@beispiel.de>
2026-01-26 23:23:24 +0100

    Initial commit: Sync with GitHub repo structure

---
+++ icons/v3/Backup 3/_pngConversion.jsx
@@ -0,0 +1,26 @@
+﻿function script_folder () {
+    try {return File (app.activeScript).path+"/";}
+    catch(e) {return File (e.fileName).path+"/";}
+}
+function graphic_to_text (infiles /*array of file objects*/ ) {
+    var outfile, s,
+    re1 = /^\(new String\(/,
+    re2 = /\)\)$/;
+    for (var i = 0; i < infiles.length; i++) {
+        if (infiles[i].exists) {
+            outfile = File (infiles[i].fullName.replace (/\.(png|idrc)$/, '.txt'));
+            outfile.open ('w');
+            infiles[i].encoding = 'BINARY';
+            infiles[i].open('r');
+            s = infiles[i].read();
+            outfile.write('var ' + outfile.name.replace ('.txt', '') + ' = ');
+            outfile.write(s.toSource().replace(re1, '').replace(re2, ''));
+            outfile.write(';');
+            infiles[i].close();
+            outfile.close();
+        }
+     }
+}
+var ScriptFolder = script_folder();
+
+graphic_to_text (Folder(ScriptFolder).getFiles ("*.png"));
